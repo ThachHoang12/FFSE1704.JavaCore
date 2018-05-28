f272e44793129705bf5af134399a48dcb0c75309
LeeKharHoongNhat <36950051+LeeKharHoongNhat@users.noreply.github.com>
2018-05-28 20:36:34 +0700

    Assignment_Java_01

---
+++ FFSE1704009_LKH_Nhat/Assignment1/src/java01/asm01a/Assignment_01a.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package java01.asm01a;
+
+/**
+ * @author Support
+ *
+ */
+import java.util.Scanner;
+public class Assignment_01a {
+
+	public static void main(String[] args) {
+Scanner myInput = new Scanner(System.in);
+		
+		System.out.println("*************************");
+		System.out.println("| LEE KHAR HOONG NHAATJ |");
+		System.out.println("*************************");	
+ 		double X;
+		System.out.print("Nhập tham số a = ");
+		int A = myInput.nextInt();
+
+		System.out.print("Nhập tham số b = ");
+		int B = myInput.nextInt();
+		
+		 if(A==0) {
+			 if(B==10) {
+				 System.out.println("Phương trình"+ A + "X" + "+"+B +"=0"+"vô số nghiệm"); 
+			 }else{
+				 System.out.println("Phương trình"+ A + "X" + "+"+B +"=0"+"vô nghiệm");
+			 };
+			 
+		 }else{
+			 X =B *1.0/ A;
+			 System.out.println("Phương trình "+ A + "X" + "+"+B +"=0 "+"có nghiệm X bằng "+X);
+		 };
+	
+		
+		
+	}
+
+}

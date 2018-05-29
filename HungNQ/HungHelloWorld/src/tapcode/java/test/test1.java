e88901ad03e70781c2cd572bba9c1866b55e44d8
Nguyễn Quốc Hưng <ffse1702016@st.fasttrack.edu.vn>
2018-05-29 09:58:02 +0700

    test

---
+++ HungNQ/HungHelloWorld/src/tapcode/java/test/test1.java
@@ -0,0 +1,18 @@
+/**
+ * code tập java
+ */
+package tapcode.java.test;
+
+/**
+ * @author Admin
+ *
+ */
+public class test1 {
+	public static void main(String[] args) {
+		int a = 5; //Khai báo biến không khởi đầu giá trị
+		double b = 7*2;
+		double c = 4/2;
+		double d = a+b-c;
+		System.out.println("Tổng: " + d);
+	}
+}

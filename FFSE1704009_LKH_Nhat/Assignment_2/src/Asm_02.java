e70c887cbd9d8c265abcc826cb4e3209054e1122
LeeKharHoongNhat <36950051+LeeKharHoongNhat@users.noreply.github.com>
2018-05-30 01:06:59 +0700

    Asm02

--- FFSE1704009_LKH_Nhat/Assignment_2/src/Asm_02.java
+++ FFSE1704009_LKH_Nhat/Assignment_2/src/Asm_02.java
@@ -1,3 +1,4 @@
+
 /************************************************* 
  * Assignment02_in_mang
  * @author: LEE KHAR HOONG NHAATJ------ASM_JAVA_02
@@ -33,70 +34,22 @@ public class Asm_02 {
 			System.out.print(array[i] + " ");
 		}
 		System.out.println(" ");
-		// tìm min max
 		int max = array[0];
-
 		int min = array[0];
-
+		int X = 0;
+		int Y = 0;
 		for (int i = 0; i < n; i++) {
 			if (max < array[i]) {
 				max = array[i];
+				X = i;
 			}
 			if (min > array[i]) {
 				min = array[i];
+				Y = i;
 			}
 		}
-		System.out.println("Giá trị lớn nhất trong dãy số là: " + max);
-		System.out.println("Giá trị nhỏ nhất trong dãy số là: " + min);
-
-		int X = max;
-		
-		int A = 0;
-		int B = n - 1;
-		
-		// System.out.println(B);
-		// nếu A còn nhỏ hơn hoặc bằng B thì còn tiếp tục thực hiện thân vòng lặp
-		
-		while (A <= B) {
-			int C = (A + B) / 2;
-
-			// nếu giá trị phần tử tại vị trí C bằng số nguyên X cần tìm
-			// thì thông báo tìm thấy số X tại vị trí C
-			// và kết thúc vòng lặp
-			if ( array[C] == X) {
-				System.out.println("Giá trị lớn nhất là " + X + " tại vị trí thứ " + C + " trong mảng");
-				
-				break; // kết thúc vòng lặp while và bỏ qua các lệnh bên dưới
-			} else {
-				if ( array[C] < X) {
-					// nếu phần tử tại C nhỏ hơn số nguyên X
-					// thì tăng A = B + 1
-					// và quay lại thực hiện vòng lặp while
-					A = C + 1;
-				} else {
-					// nếu phần tử tại C lớn hơn số nguyên X
-					// thì giảm B = C - 1
-					// và quay lại thực hiện vòng lặp while
-					B = C - 1;
-				}
-			}
-		};
-		int Y = min;
-		int a = 0;
-		int b = n - 1;
-		while (a <= b) {
-			int D = (a + b) / 2;
-			if ( array[D] == Y) {				
-				System.out.println("Giá trị nhỏ nhất là " + Y + " tại vị trí thứ " + D + " trong mảng");
-				break; 
-			} else {
-				if ( array[D] < Y) {
-					a = D + 1;
-				} else {
-					b = D - 1;
-				}
-			}
-		};
+		System.out.println("Giá Trị Lớn nhất là " + max + " Vị Trí của số lớn nhất là : " + X);
+		System.out.println("Giá Trị Nhỏ nhất là " + min + " Vị Trí của số nhỏ nhất là : " + Y);
 	}
 
 }
